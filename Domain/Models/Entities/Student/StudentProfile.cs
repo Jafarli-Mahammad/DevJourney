67b7f2ac1b30056609909fa4d7e65434f91a528d
Mahammad Jafarli <jafarli.mahammad.dev@gmail.com>
2026-08-06 19:11:36 +0400

    refactor(StudentProfile): update properties and comments

--- Domain/Models/Entities/Student/StudentProfile.cs
+++ Domain/Models/Entities/Student/StudentProfile.cs
@@ -8,30 +8,31 @@ namespace Domain.Models.Entities.Student
         public Guid ApplicationUserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int Age { get; set; }
+        public string Email { get; set; }
+        public int? Age { get; set; }
         public string? Location { get; set; }
-        //public PrimaryRole Role { get; set; }
         public string? CVUrl { get; set; }
         public string? LinkedinUrl { get; set; }
         public string? GitHubUrl { get; set; }
-        public ExperienceLevel Experience { get; set; }
-        public string Achievements { get; set; }
         public string? Bio { get; set; }
-        public WorkFormat PreferredWorkFormat { get; set; }
+        //public PrimaryRole Role { get; set; }
+        /*public ExperienceLevel Experience { get; set; }*/
+        /*public string Achievements { get; set; }*/
+        /*public WorkFormat PreferredWorkFormat { get; set; }*/
 
-        private readonly List<StudentSkill> _studentSkills = new();
-        private readonly List<StudentLanguage> _studentLanguages = new();
+        //private readonly List<StudentSkill> _studentSkills = new();
+        /*private readonly List<StudentLanguage> _studentLanguages = new();*/
 
-        public IReadOnlyCollection<StudentSkill> StudentSkills => _studentSkills.AsReadOnly();
-        public IReadOnlyCollection<StudentLanguage> StudentLanguages => _studentLanguages.AsReadOnly();
+        //public IReadOnlyCollection<StudentSkill> StudentSkills => _studentSkills.AsReadOnly();
+        //public IReadOnlyCollection<StudentLanguage> StudentLanguages => _studentLanguages.AsReadOnly();
 
-        public void AddSkill(Guid skillId)
+        /*public void AddSkill(Guid skillId)
         {
             if (_studentSkills.Any(s => s.SkillId == skillId)) return;
             _studentSkills.Add(new StudentSkill { SkillId = skillId, StudentProfileId = Id });
-        }
+        }*/
 
-        public void AddLanguage(Guid languageId, LanguageProficiencyLevel level)
+        /*public void AddLanguage(Guid languageId, LanguageProficiencyLevel level)
         {
             if (_studentLanguages.Any(l => l.LanguageId == languageId)) return;
             _studentLanguages.Add(new StudentLanguage
@@ -40,6 +41,6 @@ namespace Domain.Models.Entities.Student
                 StudentProfileId = Id,
                 ProficiencyLevel = level
             });
-        }
+        }*/
     }
 }
